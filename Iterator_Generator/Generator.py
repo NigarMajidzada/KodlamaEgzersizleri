67c5cca4f775c563393baccc3dc06ab7b01e8be2
NigarMajidzada <107548352+NigarMajidzada@users.noreply.github.com>
2023-12-13 16:24:38 +0400

    Initial commit

--- Iterator_Generator/Generator.py
+++ Iterator_Generator/Generator.py
@@ -1,9 +1,22 @@
-#
-
+#generator kullanmadan
 def kareleri_al():
     sonuc=[]
     for i in range(1,6):
         sonuc.append(i**2)
     return sonuc
-#
-print(kareleri_al())
+# print(kareleri_al())
+
+#generatorla
+
+def kareleri_al2():
+    for i in range(1,6):
+        yield i**2
+
+generator=kareleri_al2()
+print(generator)
+iterator=iter(generator)
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
